b67a78fad36d7cb6f612a50156efb88b6ae44838
MD. RISHADUL ISLAM KHAN <rishadulislam@outlook.com>
2019-09-17 10:10:48 +0600

    login page is designed
    the contoller path is not set. trying to find out the corrent path

--- HrManagementSystem/src/main/java/controllers/LoginController.java
+++ HrManagementSystem/src/main/java/controllers/LoginController.java
@@ -10,46 +10,59 @@ import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginController implements Initializable {
     
     @FXML
-    private Label label;
+    private TextField email;
+    private TextField password;
+    private Button loginButton;
+    private Label errorLabel;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        Connection connection = null;
-        try {
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try {
-            connection = DriverManager.getConnection(
-                    "jdbc:sqlserver://localhost:1433;databaseName=HRManagementSystem;selectMethod=cursor",
-                    "sa", "123456");
-        } catch (SQLException ex) {
-            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        try {
-            System.out.println(connection.getMetaData().getDatabaseProductName());
-            
-            if(connection.getMetaData().getDatabaseProductName().equals("Microsoft SQL Server")){
-                label.setText("Connection established");
-            }else{
-                label.setText("Some problem occured during connection");
-            }
-        } catch (SQLException ex) {
-
-        }
-        System.out.println("You clicked me!");
-    }
+//    @FXML
+//    private void handleButtonAction(ActionEvent event) {
+//        Connection connection = null;
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        } catch (ClassNotFoundException ex) {
+//            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        try {
+//            connection = DriverManager.getConnection(
+//                    "jdbc:sqlserver://localhost:1433;databaseName=HRManagementSystem;selectMethod=cursor",
+//                    "sa", "123456");
+//        } catch (SQLException ex) {
+//            Logger.getLogger(LoginController.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        try {
+//            System.out.println(connection.getMetaData().getDatabaseProductName());
+//            
+//            if(connection.getMetaData().getDatabaseProductName().equals("Microsoft SQL Server")){
+//                label.setText("Connection established");
+//            }else{
+//                label.setText("Some problem occured during connection");
+//            }
+//        } catch (SQLException ex) {
+//
+//        }
+//        System.out.println("You clicked me!");
+//    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        loginButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                errorLabel.setText("checked");
+                email.setText("checked");
+                password.setText("checked");
+            }
+        });
     }    
 }
--- HrManagementSystem/src/main/java/utils/MainApp.java
+++ HrManagementSystem/src/main/java/utils/MainApp.java
@@ -19,6 +19,7 @@ public class MainApp extends Application {
         
         stage.setTitle("JavaFX and Maven");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
